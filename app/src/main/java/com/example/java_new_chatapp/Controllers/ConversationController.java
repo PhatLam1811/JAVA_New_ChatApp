a50e44f2d3505a5ded8063509bf60b7c2e2ceff4
phatlam1811 <phatlam1811@gmail.com>
2024-03-01 08:51:55 +0700

    Add basic controllers

---
+++ app/src/main/java/com/example/java_new_chatapp/Controllers/ConversationController.java
@@ -0,0 +1,5 @@
+package com.example.java_new_chatapp.Controllers;
+
+public class ConversationController {
+
+}
---
+++ app/src/main/java/com/example/java_new_chatapp/Controllers/UserController.java
@@ -0,0 +1,4 @@
+package com.example.java_new_chatapp.Controllers;
+
+public class UserController {
+}
