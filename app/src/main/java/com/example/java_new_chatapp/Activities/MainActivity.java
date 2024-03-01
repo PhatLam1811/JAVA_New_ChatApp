8de3a81e0f0d0b7e7fb735a96f93d5ae6e5cfa7d
phatlam1811 <phatlam1811@gmail.com>
2024-03-01 08:44:13 +0700

    Init Proj

---
+++ app/src/main/java/com/example/java_new_chatapp/Activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.java_new_chatapp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.java_new_chatapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
