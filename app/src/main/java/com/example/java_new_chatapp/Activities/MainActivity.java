4482013eb34c3b2ee5cdf5083e4565ede7dc45ee
DESKTOP-OA1VINS\PC <phatlam1811@gmail.com>
2024-03-05 14:11:47 +0700

    Update some test for binding logic & view

--- app/src/main/java/com/example/java_new_chatapp/Activities/MainActivity.java
+++ app/src/main/java/com/example/java_new_chatapp/Activities/MainActivity.java
@@ -1,16 +1,20 @@
 package com.example.java_new_chatapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
-import com.example.java_new_chatapp.R;
+import com.example.java_new_chatapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding m_binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // initialized view binding
+        this.m_binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(this.m_binding.getRoot());
+
+        this.m_binding.textLog.setText("No more hello world!");
     }
 }
