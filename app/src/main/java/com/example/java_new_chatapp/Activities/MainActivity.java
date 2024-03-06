04fa5a97f67248ab1594788c9c8edfc4ddd2fe23
DESKTOP-OA1VINS\PC <phatlam1811@gmail.com>
2024-03-06 13:48:32 +0700

    App now can listen to database

--- app/src/main/java/com/example/java_new_chatapp/Activities/MainActivity.java
+++ app/src/main/java/com/example/java_new_chatapp/Activities/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.java_new_chatapp.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
 import com.example.java_new_chatapp.databinding.ActivityMainBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ValueEventListener {
     private ActivityMainBinding m_binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +22,19 @@ public class MainActivity extends AppCompatActivity {
         this.m_binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(this.m_binding.getRoot());
 
-        this.m_binding.textLog.setText("No more hello world!");
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference dataRef = database.getReference("Message");
+
+        dataRef.addValueEventListener(this);
+    }
+
+    @Override
+    public void onDataChange(@NonNull DataSnapshot snapshot) {
+        Log.d("[DEBUG]", "database changed!");
+    }
+
+    @Override
+    public void onCancelled(@NonNull DatabaseError error) {
+
     }
 }
