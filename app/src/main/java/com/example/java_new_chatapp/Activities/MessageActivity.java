9084826f88ad69182d25b49df2731a6dc994fd64
DESKTOP-OA1VINS\PC <phatlam1811@gmail.com>
2024-03-08 14:29:16 +0700

    Implement get all messages on start message activity

--- app/src/main/java/com/example/java_new_chatapp/Activities/MessageActivity.java
+++ app/src/main/java/com/example/java_new_chatapp/Activities/MessageActivity.java
@@ -1,16 +1,29 @@
 package com.example.java_new_chatapp.Activities;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.java_new_chatapp.Firebase.FirebaseManager;
+import com.example.java_new_chatapp.Controllers.ConversationController;
 import com.example.java_new_chatapp.Models.Message;
+import com.example.java_new_chatapp.Models.User;
 import com.example.java_new_chatapp.databinding.ActivityMessageBinding;
 
+import java.util.ArrayList;
+
 public class MessageActivity extends AppCompatActivity {
+    // bindings
     private ActivityMessageBinding m_binding;
+
+    // controller
+    private ConversationController m_controller;
+
+    // users
+    private ArrayList<User> m_participants;
+    private int m_userIndex;
+
+    // messages
+    private ArrayList<Message> m_messages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +33,26 @@ public class MessageActivity extends AppCompatActivity {
         this.m_binding =ActivityMessageBinding.inflate(this.getLayoutInflater());
         setContentView(this.m_binding.getRoot());
 
-        // generate first message
-        Message firstMessage = Message.generateFakeMessage();
+        // init controller
+        this.m_controller = new ConversationController();
+        this.m_controller.init();
 
-        // upload first message
-        FirebaseManager.getInstance().getFirestore()
-                .collection("Messages")
-                .add(firstMessage)
-                .addOnSuccessListener(p -> Log.d("[DEBUG]", "upload message successful!"))
-                .addOnFailureListener(p -> Log.d("[DEBUG]", "upload message failed!"));
+        // init users
+        this.m_participants = new ArrayList<User>();
+        this.m_userIndex = -1;
+
+        // load all messages of the conversation
+        // TO-DO: only load recent messages (10-15) instead of loading all
+        this.m_messages = this.m_controller.getAllMessages();
+
+//        // generate first message
+//        Message firstMessage = Message.generateFakeMessage();
+//
+//        // upload first message
+//        FirebaseManager.getInstance().getFirestore()
+//                .collection("Messages")
+//                .add(firstMessage)
+//                .addOnSuccessListener(p -> Log.d("[DEBUG]", "upload message successful!"))
+//                .addOnFailureListener(p -> Log.d("[DEBUG]", "upload message failed!"));
     }
 }
--- app/src/main/java/com/example/java_new_chatapp/Controllers/ConversationController.java
+++ app/src/main/java/com/example/java_new_chatapp/Controllers/ConversationController.java
@@ -1,5 +1,41 @@
 package com.example.java_new_chatapp.Controllers;
 
-public class ConversationController {
+import android.util.Log;
 
+import androidx.annotation.NonNull;
+
+import com.example.java_new_chatapp.Models.Message;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.firestore.AggregateQuerySnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class ConversationController {
+    // firestore
+    private FirebaseFirestore m_firestore;
+    private final String _messageCollection = "Messages";
+
+    public void init() {
+        this.m_firestore = FirebaseFirestore.getInstance();
+    }
+
+    public ArrayList<Message> getAllMessages() {
+        ArrayList<Message> messages = new ArrayList<Message>();
+
+        this.m_firestore
+                .collection(_messageCollection)
+                .get()
+                .addOnSuccessListener(snapshots -> {
+                    Log.d("[DEBUG]", snapshots.getDocuments().toString());
+                })
+                .addOnFailureListener(error -> Log.d("[DEBUG]", Objects.requireNonNull(error.getMessage())));
+
+        return messages;
+    }
 }
