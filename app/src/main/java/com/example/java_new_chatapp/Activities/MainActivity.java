7dd1f7f7e03fabbd8023901238f52e606a59e08d
DESKTOP-OA1VINS\PC <phatlam1811@gmail.com>
2024-03-08 13:52:21 +0700

    Change startup activity to MessageActivity

--- app/src/main/java/com/example/java_new_chatapp/Activities/MainActivity.java
+++ app/src/main/java/com/example/java_new_chatapp/Activities/MainActivity.java
@@ -2,11 +2,7 @@ package com.example.java_new_chatapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
-// binding
-import com.example.java_new_chatapp.Firebase.FirebaseManager;
-import com.example.java_new_chatapp.Models.Message;
 import com.example.java_new_chatapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,15 +15,5 @@ public class MainActivity extends AppCompatActivity {
         // init view binding
         this.m_binding = ActivityMainBinding.inflate(this.getLayoutInflater());
         setContentView(this.m_binding.getRoot());
-
-        // generate first message
-        Message firstMessage = Message.generateFakeMessage();
-
-        // upload first message
-        FirebaseManager.getInstance().getFirestore()
-                .collection("Messages")
-                .add(firstMessage)
-                .addOnSuccessListener(p -> Log.d("[DEBUG]", "upload message successful!"))
-                .addOnFailureListener(p -> Log.d("[DEBUG]", "upload message failed!"));
     }
 }
--- app/src/main/java/com/example/java_new_chatapp/Activities/MessageActivity.java
+++ app/src/main/java/com/example/java_new_chatapp/Activities/MessageActivity.java
@@ -1,9 +1,12 @@
 package com.example.java_new_chatapp.Activities;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.java_new_chatapp.Firebase.FirebaseManager;
+import com.example.java_new_chatapp.Models.Message;
 import com.example.java_new_chatapp.databinding.ActivityMessageBinding;
 
 public class MessageActivity extends AppCompatActivity {
@@ -16,5 +19,15 @@ public class MessageActivity extends AppCompatActivity {
         // init view binding
         this.m_binding =ActivityMessageBinding.inflate(this.getLayoutInflater());
         setContentView(this.m_binding.getRoot());
+
+        // generate first message
+        Message firstMessage = Message.generateFakeMessage();
+
+        // upload first message
+        FirebaseManager.getInstance().getFirestore()
+                .collection("Messages")
+                .add(firstMessage)
+                .addOnSuccessListener(p -> Log.d("[DEBUG]", "upload message successful!"))
+                .addOnFailureListener(p -> Log.d("[DEBUG]", "upload message failed!"));
     }
 }
