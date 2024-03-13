92db8b81a9dd4b9c1ad2802bc74c9df17fbee01b
phatlam1811 <phatlam1811@gmail.com>
2024-03-14 01:28:26 +0700

    scroll to last message (not working with initial startup)

--- app/src/main/java/com/example/java_new_chatapp/Activities/ConversationActivity.java
+++ app/src/main/java/com/example/java_new_chatapp/Activities/ConversationActivity.java
@@ -1,11 +1,15 @@
 package com.example.java_new_chatapp.Activities;
 
 import android.os.Bundle;
+import android.os.Debug;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearSmoothScroller;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.java_new_chatapp.Adapters.MessageAdapter;
 import com.example.java_new_chatapp.Models.Message;
@@ -24,14 +28,12 @@ import com.google.firebase.firestore.QuerySnapshot;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Comparator;
-import java.util.List;
 
 public class ConversationActivity extends AppCompatActivity {
     // bindings
     protected ActivityConversationBinding m_binding;
     protected MessageAdapter m_messageAdapter;
-    protected Button m_btnSendMessage;
-    protected EditText m_editTxtMessage;
+    protected RecyclerView.SmoothScroller m_messagesSmoothScroller;
 
     // participants
     protected ArrayList<User> m_participants;
@@ -53,6 +55,33 @@ public class ConversationActivity extends AppCompatActivity {
         int partnerIndex = (this.m_appUserIndex + 1) % this.m_participants.size();
         return this.m_participants.get(partnerIndex);
     }
+
+    public Button getBtnSendMessage() {
+        return this.m_binding.btnSendMessage;
+    }
+
+    public EditText getEditTxtMessage() {
+        return this.m_binding.editTxtMessage;
+    }
+
+    public RecyclerView getRecyclerViewMessage() {
+        return this.m_binding.recyclerViewMessages;
+    }
+
+    public RecyclerView.SmoothScroller getMessagesSmoothScroller() {
+        if (this.m_messagesSmoothScroller == null)
+        {
+            this.m_messagesSmoothScroller = new LinearSmoothScroller(this.getRecyclerViewMessage().getContext()) {
+                // Override computeScrollVectorForPosition to customize scrolling direction
+                @Override
+                protected int getVerticalSnapPreference() {
+                    return SNAP_TO_START; // Or SNAP_TO_END, SNAP_TO_ANY
+                }
+            };
+        }
+        return m_messagesSmoothScroller;
+    }
+
     // endregion
 
     // region activity events
@@ -65,11 +94,7 @@ public class ConversationActivity extends AppCompatActivity {
         setContentView(this.m_binding.getRoot());
 
         // button send message
-        this.m_btnSendMessage = this.m_binding.btnSendMessage;
-        this.m_btnSendMessage.setOnClickListener(this::onBtnSendClicked);
-
-        // message text box
-        this.m_editTxtMessage = this.m_binding.editTxtMessage;
+        this.getBtnSendMessage().setOnClickListener(this::onBtnSendClicked);
 
         // fake conversation participants
         User fakeAppUser = User.getFakeAppUser();
@@ -87,6 +112,14 @@ public class ConversationActivity extends AppCompatActivity {
         // message adapter
         this.m_messageAdapter = new MessageAdapter(this.m_messages);
         this.m_binding.recyclerViewMessages.setAdapter(this.m_messageAdapter);
+        this.m_messageAdapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
+            @Override
+            public void onItemRangeChanged(int positionStart, int itemCount) {
+                super.onItemRangeChanged(positionStart, itemCount);
+                Log.d(AppDefines.Log.TAG_DEBUG, "on dataset changed");
+                scrollToLatestMessage();
+            }
+        });
 
         // init firestore
         this.m_firestoreDb = FirebaseFirestore.getInstance();
@@ -98,14 +131,14 @@ public class ConversationActivity extends AppCompatActivity {
     // region callbacks
     protected void onBtnSendClicked(View btnSendMessage) {
         android.util.Log.d(AppDefines.Log.TAG_DEBUG, "btn send message clicked!!!");
-        android.util.Log.d(AppDefines.Log.TAG_DEBUG, "message: " + this.m_editTxtMessage.getText());
+        android.util.Log.d(AppDefines.Log.TAG_DEBUG, "message: " + this.getEditTxtMessage().getText());
 
         // create new message object
         Message newMessage = new Message
                 (
                     this.getAppUser(),
                     this.getPartner(),
-                    this.m_editTxtMessage.getText().toString(),
+                    this.getEditTxtMessage().getText().toString(),
                     Calendar.getInstance().getTime()
                 );
 
@@ -159,5 +192,21 @@ public class ConversationActivity extends AppCompatActivity {
         this.addMessages(newMessage);
 
         this.m_messageAdapter.notifyItemRangeChanged(0, this.m_messages.size());
+    }
+
+    protected void scrollToLatestMessage() {
+        int latestMessageIndex = this.m_messages.size() - 1;
+
+        RecyclerView messageRecyclerView = this.getRecyclerViewMessage();
+        RecyclerView.LayoutManager messageLayoutManager = messageRecyclerView.getLayoutManager();
+        RecyclerView.SmoothScroller messageSmoothScroller = this.getMessagesSmoothScroller();
+
+        messageSmoothScroller.setTargetPosition(latestMessageIndex);
+
+        if (messageLayoutManager != null)
+        {
+            messageLayoutManager.startSmoothScroll(messageSmoothScroller);
+            Log.d(AppDefines.Log.TAG_DEBUG, "should be able to scroll");
+        }
     }
 }
