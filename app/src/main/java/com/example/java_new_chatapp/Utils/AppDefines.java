804b790870df81b3566e7b957b09ea0a266690ca
phatlam1811 <phatlam1811@gmail.com>
2024-03-13 07:10:06 +0700

    How can it still be here????

--- app/src/main/java/com/example/java_new_chatapp/Utils/AppDefines.java
+++ app/src/main/java/com/example/java_new_chatapp/Utils/AppDefines.java
@@ -1,2 +1,8 @@
-package com.example.java_new_chatapp.Utils;public class AppDefines {
+package com.example.java_new_chatapp.Utils;
+
+public class AppDefines {
+    public static class Log {
+        public static final String TAG_DEBUG = "[DEBUG]";
+        public static final String TAG_ERROR = "[ERROR]";
+    }
 }
